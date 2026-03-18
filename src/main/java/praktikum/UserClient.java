ff9419a38ee91315e827f2ba111b5b3811219fc0
Islam Kitarov <islamidzeee@yandex.ru>
2026-03-18 16:55:41 +0500

    3

--- src/main/java/praktikum/UserClient.java
+++ src/main/java/praktikum/UserClient.java
@@ -1,23 +1,28 @@
 package praktikum;
 
+import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
 import static io.restassured.RestAssured.given;
 
 public class UserClient {
     private static final String BASE_URL = "https://stellarburgers.education-services.ru/api/auth/";
 
+    @Step("Создание пользователя")
     public ValidatableResponse create(User user) {
         return given()
                 .header("Content-type", "application/json")
                 .body(user)
+                .when()
                 .post(BASE_URL + "register")
                 .then();
     }
 
+    @Step("Удаление пользователя")
     public void delete(String accessToken) {
         if (accessToken != null) {
             given()
                     .header("Authorization", accessToken)
+                    .when()
                     .delete(BASE_URL + "user")
                     .then();
         }
